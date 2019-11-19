de4caca613161f572d804caecbcf557975b6a706
Taylor Cool <taycool@gmail.com>
2019-11-19 11:27:50 -0800

    updated dashboard, pulled master

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,49 +1,94 @@
-import React from "react";
-import { BrowserRouter as Link } from "react-router-dom";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import Paper from "@material-ui/core/Paper";
-import Grid from '@material-ui/core/Grid';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
 const Dashboard = () => {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
 
-    const useStyles = makeStyles(theme => ({
-        root: {
-            display: "flex",
-            flexWrap: "wrap",
-            flexDirection: "column"
-        },
-        margin: {
-            margin: theme.spacing(1)
-        },
-        withoutLabel: {
-            marginTop: theme.spacing(3)
-        },
-        textField: {
-            width: 375
-        }
-        }));
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
 
-    const classes = useStyles();
-
-    return (
-        <div className={classes.root}>
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}>
-                        User Card
-                    </Paper>
-                </Grid>
-                <Grid item xs={12}>
-                    <Paper className={classes.paper}>
-                        View Cards
-                    </Paper>
-                </Grid>
-
-            </Grid>
-        </div>
-    )
+  return (
+    <div className='container'>
+        <Card className={classes.card}>
+        <CardHeader
+            action={
+            <IconButton aria-label="settings">
+                <MoreVertIcon />
+            </IconButton>
+            }
+        />
+        <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+            The User's business card should go here
+            </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+            <IconButton
+                className={clsx(classes.expand, {
+                    [classes.expandOpen]: expanded,
+                })}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show QR code"
+            >
+            <ExpandMoreIcon />
+            </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+            <Typography paragraph>
+                QR code here!
+            </Typography>
+            <CardMedia
+                className={classes.media}
+                image="src/Images/wikipediaQR.svg"
+                title="Wikipedia QR code"
+            />
+            </CardContent>
+        </Collapse>
+        </Card>
+    </div>
+  );
 }
 
 export default Dashboard;
