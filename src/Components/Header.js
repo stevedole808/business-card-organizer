85f8a80574570e1149f42944591dded078a9dbb7
Adam Reynolds <51489495+A-Lawrence-Reynolds@users.noreply.github.com>
2019-11-19 15:19:21 -0800

    Revert "Adam reynolds"

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,43 +1,43 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter as NavLink } from "react-router-dom";
-import { fade, makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import MenuItem from "@material-ui/core/MenuItem";
-import Menu from "@material-ui/core/Menu";
-import AccountCircle from "@material-ui/icons/AccountCircle";
-import MoreIcon from "@material-ui/icons/MoreVert";
-import logo from "/Users/taylorcool/Documents/GitHub/front-end/src/Images/meishi1.jpg";
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import logo from '/Users/taylorcool/Documents/GitHub/front-end/src/Images/meishi1.jpg';
 
 const useStyles = makeStyles(theme => ({
   grow: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(2)
+    marginRight: theme.spacing(2),
   },
   title: {
-    display: "none",
-    [theme.breakpoints.up("sm")]: {
-      display: "block"
-    }
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
   },
   inputRoot: {
-    color: "inherit"
+    color: 'inherit',
   },
   sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex"
-    }
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
   },
   sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none"
-    }
-  }
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
 }));
 
 const Header = () => {
@@ -65,47 +65,49 @@ const Header = () => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  const menuId = "primary-search-account-menu";
+  const menuId = 'primary-search-account-menu';
   const renderNavLoggedIn = (
     <>
-      <NavLink to="/" className="nav-link">
+    <NavLink to='/' className='nav-link'>
         <MenuItem onClick={handleMenuClose}>My Profile</MenuItem>
-      </NavLink>
-      <NavLink to="/" className="nav-link">
+    </NavLink>
+    <NavLink to='/' className='nav-link'>
         <MenuItem onClick={handleMenuClose}>Log Out</MenuItem>
-      </NavLink>
+    </NavLink>
     </>
   );
   const renderNavLoggedOut = (
     <>
-      <NavLink to="/login" className="nav-link">
+    <NavLink to='/login' className='nav-link'>
         <MenuItem onClick={handleMenuClose}>Log In</MenuItem>
-      </NavLink>
+    </NavLink>
     </>
   );
 
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
       id={menuId}
       keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
-      {localStorage.getItem("token") ? renderNavLoggedIn : renderNavLoggedOut}
+    
+    {localStorage.getItem("token") ? renderNavLoggedIn : renderNavLoggedOut}
+
     </Menu>
   );
 
-  const mobileMenuId = "primary-search-account-menu-mobile";
+  const mobileMenuId = 'primary-search-account-menu-mobile';
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
       id={mobileMenuId}
       keepMounted
-      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
     >
@@ -126,8 +128,8 @@ const Header = () => {
     <div className={`${classes.grow} header`}>
       <AppBar position="static">
         <Toolbar>
-          <img src={logo} alt="Meishi logo" />
-
+        <img src={logo} alt='Meishi logo' />
+        
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
             <IconButton
@@ -158,5 +160,5 @@ const Header = () => {
       {renderMenu}
     </div>
   );
-};
+}
 export default Header;
